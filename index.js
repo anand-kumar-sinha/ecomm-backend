2bc91198f8a09216966d135ea96ca2eb7cd6157d
anand kumar <anandkumar7368970125@gmail.com>
2025-04-18 13:24:40 +0530

    nothing

--- index.js
+++ index.js
@@ -42,19 +42,6 @@ app.use("/api/order", orderRouter);
     res.send('Api working')
 })*/
 
-app.get("/", async (req, res, next) => {
-  try {
-    let html = fs.readFileSync(path.resolve(root, "index.html"), "utf-8");
-
-    // Transform HTML using Vite plugins.
-    html = await viteServer.transformIndexHtml(req.url, html);
-
-    res.send(html);
-  } catch (e) {
-    return next(e);
-  }
-});
-
 // Activate
 app.listen(PORT, () => {
   console.log("Server Started at : " + PORT);
