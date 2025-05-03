a59d0b3c9946e44854500d002907ac9e7d593022
anand kumar <anandkumar7368970125@gmail.com>
2025-05-03 20:16:11 +0530

    fix:- my profile

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,12 +5,13 @@ import {
   adminUser,
   myProfile,
 } from "../controllers/userController.js";
+import authUser from "../middleware/auth.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/login", loginUser);
 userRouter.post("/register", registerUser);
 userRouter.post("/admin/login", adminUser);
-userRouter.post("/me", myProfile);
+userRouter.post("/me",authUser, myProfile);
 
 export default userRouter;
