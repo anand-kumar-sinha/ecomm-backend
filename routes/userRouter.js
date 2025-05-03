6c7a834c786a60d9f164bf081700984b24a09ce5
anand kumar <anandkumar7368970125@gmail.com>
2025-05-03 20:13:24 +0530

    add:- my profile

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,12 +1,16 @@
-import express from 'express';
-import {loginUser,registerUser, adminUser} from '../controllers/userController.js'
+import express from "express";
+import {
+  loginUser,
+  registerUser,
+  adminUser,
+  myProfile,
+} from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
-userRouter.post('/login',loginUser)
-userRouter.post('/register',registerUser)
-userRouter.post('/admin/login',adminUser)
-
-
+userRouter.post("/login", loginUser);
+userRouter.post("/register", registerUser);
+userRouter.post("/admin/login", adminUser);
+userRouter.post("/me", myProfile);
 
 export default userRouter;
