1a8ef8010b19ab6a52ee821fb7bcc32ce7a4fcf5
anand kumar <anandkumar7368970125@gmail.com>
2025-05-06 09:24:01 +0530

    add:- fetch whislist

--- controllers/whislistController.js
+++ controllers/whislistController.js
@@ -4,7 +4,7 @@ const addWishlist = async (req, res) => {
   try {
     const { userId, productId } = req.body;
     const userData = await userModel.findById(userId).select("wishlist");
-    if(!userData) {
+    if (!userData) {
       return res.status(404).json({
         success: false,
         message: "User not found",
@@ -28,7 +28,7 @@ const removeWhislist = async (req, res) => {
   try {
     const { userId, productId } = req.body;
     const userData = await userModel.findById(userId).select("wishlist");
-    if(!userData) {
+    if (!userData) {
       return res.status(404).json({
         success: false,
         message: "User not found",
@@ -51,4 +51,46 @@ const removeWhislist = async (req, res) => {
   }
 };
 
-export { addWishlist, removeWhislist };
+const wishlistFetch = async (req, res) => {
+  try {
+    const { userId, page } = req.body;
+    if (!userId) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+    if (!page) {
+      page = 1;
+    }
+    const total = 10;
+    const userData = await userModel
+      .findById(userId)
+      .select("wishlist")
+      .populate({
+        path: "wishlist",
+        options: {
+          skip: (parseInt(page) - 1) * total,
+          limit: total,
+        },
+      });
+
+    if (!userData) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    res.json({
+      success: true,
+      wishlist: userData.wishlist,
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export { addWishlist, removeWhislist, wishlistFetch };
